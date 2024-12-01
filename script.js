70d612aa3edcdeb3d15472fc16848b8ddbf307cd
mehtashish <mehta.mtj@gmail.com>
2024-12-02 00:12:20 +0530

    functionalities updated

--- script.js
+++ script.js
@@ -32,27 +32,33 @@ async function getRepos(username) {
 }
 
 function createUserCard(user) {
-    const userID = user.name || user.login
-    const userBio = user.bio ? `<p>${user.bio}</p>` : ''
-    const cardHTML = `
-    <div class="card">
-    <div>
-      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
-    </div>
-    <div class="user-info">
-      <h2>${userID}</h2>
-      ${userBio}
-      <ul>
-        <li>${user.followers} <strong>Followers</strong></li>
-        <li>${user.following} <strong>Following</strong></li>
-        <li>${user.public_repos} <strong>Repos</strong></li>
-      </ul>
+    const userID = user.name || user.login;
+    const userBio = user.bio ? `<p>${user.bio}</p>` : '';
+    const profileLink = user.html_url;
+    const followersLink = `${profileLink}?tab=followers`;
+    const followingLink = `${profileLink}?tab=following`;
+    const repoLink = `${profileLink}?tab=repositories`;
 
-      <div id="repos"></div>
+    const cardHTML = `
+    <div class="card profile-card">
+        <div class="avatar-container">
+            <a href="${profileLink}" target="_blank">
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </a>
+        </div>
+        <div class="user-info">
+            <h1><a href="${profileLink}" target="_blank">${userID}</a></h1>
+            ${userBio}
+            <ul>
+                <li><a href="${followersLink}" target="_blank">${user.followers} Followers</a></li>
+                <li><a href="${followingLink}" target="_blank">${user.following} Following</a></li>
+                <li><a href="${repoLink}" target="_blank">${user.public_repos} Repositories</a></li>
+            </ul>
+            <div id="repos"></div>
+        </div>
     </div>
-  </div>
-    `
-    main.innerHTML = cardHTML
+    `;
+    main.innerHTML = cardHTML;
 }
 
 function createErrorCard(msg) {
@@ -66,19 +72,17 @@ function createErrorCard(msg) {
 }
 
 function addReposToCard(repos) {
-    const reposEl = document.getElementById('repos')
+    const reposEl = document.getElementById('repos');
 
-    repos
-        .slice(0, 5)
-        .forEach(repo => {
-            const repoEl = document.createElement('a')
-            repoEl.classList.add('repo')
-            repoEl.href = repo.html_url
-            repoEl.target = '_blank'
-            repoEl.innerText = repo.name
+    repos.slice(0, 5).forEach((repo) => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
 
-            reposEl.appendChild(repoEl)
-        })
+        reposEl.appendChild(repoEl);
+    });
 }
 
 async function notifySearch(username) {
